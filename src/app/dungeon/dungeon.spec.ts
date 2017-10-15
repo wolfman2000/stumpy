624662c59ee843d63b2f090c2d0070d5e54a2261
Jason Felds <jafelds@gmail.com>
2017-10-15 11:16:13 -0400

    Get dungeon items set up before the components. (#9)

---
+++ src/app/dungeon/dungeon.spec.ts
@@ -0,0 +1,106 @@
+import { Dungeon } from './dungeon';
+import { EntranceLock } from './entrance-lock';
+
+describe( 'A dungeon with no chests of randomized items and no entrance lock', () => {
+  let dungeon: Dungeon;
+
+  beforeEach( () => { dungeon = new Dungeon(0); });
+
+  it( 'has a max chest count of zero forever.', () => {
+    expect( dungeon.maxChests ).toEqual( 0 );
+  });
+
+  it( 'has a count of zero chests at the start.', () => {
+    expect( dungeon.chestCount ).toEqual( 0 );
+  });
+
+  it( 'starts with no entrance lock.', () => {
+    expect( dungeon.entranceLock ).toBe( EntranceLock.None );
+  });
+
+  it( 'cannot have its chest count decremented.', () => {
+    dungeon.decrementChestCount();
+
+    expect( dungeon.chestCount ).toEqual( 0 );
+  });
+
+  it( 'cannot have its entrance requirement changed.', () => {
+    dungeon.cycleEntranceLock();
+
+    expect( dungeon.entranceLock ).toBe( EntranceLock.None );
+  });
+});
+
+describe( 'A dungeon with two items inside', () => {
+  let dungeon: Dungeon;
+
+  beforeEach( () => { dungeon = new Dungeon(2); });
+
+  it( 'starts with a count of two chests available.', () => {
+    expect( dungeon.chestCount ).toEqual( 2 );
+  });
+
+  it( 'can have an item taken from the dungeon, updating its counts correctly.', () => {
+    dungeon.decrementChestCount();
+
+    expect( dungeon.chestCount ).toEqual( 1 );
+    expect( dungeon.maxChests ).toEqual( 2 );
+  });
+
+  it( 'can have both items taken from the dungeon, updating its counts correctly.', () => {
+    dungeon.decrementChestCount();
+    dungeon.decrementChestCount();
+
+    expect( dungeon.chestCount ).toEqual( 0 );
+    expect( dungeon.maxChests ).toEqual( 2 );
+  });
+
+  it( 'can have both items put back into the dungeon, updating its counts correctly.', () => {
+    dungeon.decrementChestCount();
+    dungeon.decrementChestCount();
+    dungeon.decrementChestCount();
+
+    expect( dungeon.chestCount ).toEqual( 2 );
+    expect( dungeon.maxChests ).toEqual( 2 );
+  });
+});
+
+describe( 'A dungeon with a medallion lock', () => {
+  let dungeon: Dungeon;
+
+  beforeEach( () => { dungeon = new Dungeon(0, EntranceLock.Unknown); });
+
+  it( 'starts in the unknown phase', () => {
+    expect( dungeon.entranceLock ).toBe( EntranceLock.Unknown );
+  });
+
+  it( 'can be cycled to Bombos through one call.', () => {
+    dungeon.cycleEntranceLock();
+
+    expect( dungeon.entranceLock ).toBe( EntranceLock.Bombos );
+  });
+
+  it( 'can be cycled to Ether through two calls.', () => {
+    dungeon.cycleEntranceLock();
+    dungeon.cycleEntranceLock();
+
+    expect( dungeon.entranceLock ).toBe( EntranceLock.Ether );
+  });
+
+  it( 'can be cycled to Quake through three calls.', () => {
+    dungeon.cycleEntranceLock();
+    dungeon.cycleEntranceLock();
+    dungeon.cycleEntranceLock();
+
+    expect( dungeon.entranceLock ).toBe( EntranceLock.Quake );
+  });
+
+  it( 'cycles back to being unknown after four calls.', () => {
+    dungeon.cycleEntranceLock();
+    dungeon.cycleEntranceLock();
+    dungeon.cycleEntranceLock();
+    dungeon.cycleEntranceLock();
+
+    expect( dungeon.entranceLock ).toBe( EntranceLock.Unknown );
+  });
+});
