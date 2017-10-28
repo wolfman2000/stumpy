a4358ec4b99134c02eaf708e443909a531e13104
Jason Felds <jafelds@gmail.com>
2017-10-28 11:00:37 -0400

    Dark world dungeons are set up on the map.
    Not every method was tested, but there is some cleanup anyway.

--- src/app/map/dungeon-locations/dungeon-location.repository.ts
+++ src/app/map/dungeon-locations/dungeon-location.repository.ts
@@ -30,5 +30,54 @@ export const DungeonLocations: Map<Location, DungeonLocation> = new Map<Location
       62,
       5.5
     )
-  ]]
+  ], [
+    Location.PalaceOfDarkness, new DungeonLocation(
+      'Palace of Darkness',
+      'lantern + bow',
+      94,
+      40
+    )
+  ], [
+    Location.SwampPalace, new DungeonLocation(
+      'Swamp Palace',
+      'mirror + flippers',
+      47,
+      91
+    )
+  ], [
+    Location.SkullWoods, new DungeonLocation(
+      'Skull Woods',
+      '',
+      6.6,
+      5.4
+    )
+  ], [
+    Location.ThievesTown, new DungeonLocation(
+      'Thieves Town',
+      '',
+      12.8,
+      47.9
+    )
+  ], [
+    Location.IcePalace, new DungeonLocation(
+      'Ice Palace',
+      '',
+      79.6,
+      85.8
+    )
+  ], [
+    Location.MiseryMire, new DungeonLocation(
+      'Misery Mire',
+      'lantern + medallion',
+      11.6,
+      82.9
+    )
+  ], [
+    Location.TurtleRock, new DungeonLocation(
+      'Turtle Rock',
+      'lantern + medallion',
+      93.8,
+      7
+    )
+  ] ]
 );
