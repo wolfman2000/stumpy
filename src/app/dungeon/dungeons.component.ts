3e88edb343eee565083e2f8193be0a89e6e36254
Jason Felds <jafelds@gmail.com>
2018-03-25 17:10:57 -0400

    Better align items and boss sections. (#80)

--- src/app/dungeon/dungeons.component.ts
+++ src/app/dungeon/dungeons.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'stumpy-dungeons',
-  templateUrl: './dungeons.component.html'
+  templateUrl: './dungeons.component.html',
+  styleUrls: ['./dungeons.component.css']
 })
 
 export class DungeonsComponent {
